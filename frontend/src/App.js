7ca80830782926d9215535129395979ea278e4c2
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-18 15:39:23 +0500

    chore - created frontend folder and dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return <NavigationBar />;
+}
+
+export default App;
