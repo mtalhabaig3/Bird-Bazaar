92c04814568ef256bffde7d2ea20b146b391e82f
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-18 16:00:11 +0500

    feature - created routed for components using react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Products from "./components/Products";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import UserProfile from "./components/UserProfile";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavigationBar />}>
+      <Route path="/" element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signUp" element={<SignUp />} />
+      <Route path="userProfile" element={<UserProfile />} />
+    </Route>
+  )
+);
 
 function App() {
-  return <NavigationBar />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,70 +1,83 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavigationBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Amazon
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Products
-              </a>
-            </li>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand" href="#">
+            Amazon
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  to={"/"}
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"products"} className="nav-link" href="#">
+                  Products
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Login
-              </a>
-            </li>
+              <li className="nav-item">
+                <Link to={"login"} className="nav-link" href="#">
+                  Login
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Cart
-              </a>
-            </li>
+              <li className="nav-item">
+                <Link to={"cart"} className="nav-link" href="#">
+                  Cart
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Account
-              </a>
-            </li>
-          </ul>
+              <li className="nav-item">
+                <Link to={"userProfile"} className="nav-link" href="#">
+                  Account
+                </Link>
+              </li>
+            </ul>
 
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-primary" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
         </div>
+      </nav>
+      <div>
+        <Outlet />
       </div>
-    </nav>
+    </div>
   );
 };
 
