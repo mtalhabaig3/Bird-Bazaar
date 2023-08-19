8963be3e2c03ab6d9c0387c7db803112fc662be5
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-19 23:29:33 +0500

    refactor - updated user schema model

--- backend/db/models/User.js
+++ backend/db/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
