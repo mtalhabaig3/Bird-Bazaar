5b3539785d80a393b861863eeee8e3a48c2d808a
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-19 23:00:33 +0500

    feature - added express middlewares

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
 
+app.use(express.urlencoded("extended"));
+app.use(express.json());
+
 app.get("/api/v1/login", (req, res) => {
   res.send("Login!");
 });
