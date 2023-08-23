b5665b012b935efa8c90eb8ea3f8898793f5a30b
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-23 16:23:48 +0500

    feature - added get products routes and logic for each one

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,53 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const User = require("./db/models/User");
+const Product = require("./db/models/Product");
 const app = express();
 const port = 5000;
+const saltRounds = 10;
 
-app.use(express.urlencoded("extended"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/api/v1/login", (req, res) => {
-  res.send("Login!");
-});
-
-app.get("/api/v1/register", async (req, res) => {
-  const { email } = req.body;
+app.post("/api/v1/login", async (req, res) => {
+  const { email, password } = req.body;
   const user = await User.findOne({ email: email }).exec();
 
   if (user) {
-    return res.status(400).json({ msg: "user already exists" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      res.status(200).json({ msg: "Login Successful!" });
+    } else {
+      res.status(401).json({ msg: "wrong credentials!" });
+    }
   }
+});
 
+app.post("/api/v1/register", async (req, res) => {
+  const { password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
   try {
-    await User.create({ ...req.body });
+    await User.create({ ...req.body, password: hashedPassword });
     res.status(200).json({ msg: "user created!" });
   } catch (error) {
     console.error(error);
   }
+});
+
+app.get("/api/v1/getProducts", async (req, res) => {
+  const products = await Product.find({});
+
+  res.status(200).send(products);
+});
+
+app.get("/api/v1/getProducts/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+
+  res.status(200).send(product);
 });
 
 async function main() {
