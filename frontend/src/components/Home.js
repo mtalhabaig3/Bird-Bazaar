38f1aba9889f6b72c57dd6cc6546423bdbfd3823
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-30 16:30:12 +0500

    refactor - removed categories

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -21,23 +21,23 @@ const featuredProducts = [
     title: "parrot_3",
     description: "This is parrot 3",
   },
-];
-
-const categories = [
   {
+    id: 4,
     image: parrotImage,
-    title: "Males",
-    description: "This is male category",
+    title: "parrot_4",
+    description: "This is parrot 4",
   },
   {
+    id: 5,
     image: parrotImage,
-    title: "Females",
-    description: "This is female category",
+    title: "parrot_5",
+    description: "This is parrot 5",
   },
   {
+    id: 6,
     image: parrotImage,
-    title: "Kids",
-    description: "This is kids category",
+    title: "parrot_6",
+    description: "This is parrot 6",
   },
 ];
 
@@ -62,37 +62,12 @@ const renderFeatured = () => {
   });
 };
 
-const renderCategories = () => {
-  return categories.map((category) => {
-    return (
-      <div>
-        <div className="col mb-5">
-          <div className="card h-100">
-            <img src={category.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{category.title}</h5>
-              <p className="card-text">{category.description}</p>
-              <Link to={"products/2"} class="btn btn-primary">
-                View details
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  });
-};
-
 const Home = () => {
   return (
     <div className="container mt-5">
       <h1>Featured</h1>
       <div className="row row-cols-1 row-cols-md-3 g-4 ">
         {renderFeatured()}
-      </div>
-      <h1>Categories</h1>
-      <div className="row row-cols-1 row-cols-md-3 g-4 ">
-        {renderCategories()}
       </div>
     </div>
   );
