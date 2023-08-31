dfc3a1d52062452199db977a526825f1c9d34b33
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-08-31 15:27:35 +0500

    feature - called get products api to fetch products

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,60 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import parrotImage from "../assets/imgs/parrotUp.JPG";
 import { Link } from "react-router-dom";
-
-const products = [
-  {
-    id: 1,
-    image: parrotImage,
-    title: "parrot_1",
-    description: "This is parrot 1",
-  },
-  {
-    id: 2,
-    image: parrotImage,
-    title: "parrot_2",
-    description: "This is parrot 2",
-  },
-  {
-    id: 3,
-    image: parrotImage,
-    title: "parrot_3",
-    description: "This is parrot 3",
-  },
-  {
-    id: 4,
-    image: parrotImage,
-    title: "parrot_4",
-    description: "This is parrot 4",
-  },
-  {
-    id: 5,
-    image: parrotImage,
-    title: "parrot_5",
-    description: "This is parrot 5",
-  },
-  {
-    id: 6,
-    image: parrotImage,
-    title: "parrot_6",
-    description: "This is parrot 6",
-  },
-];
+import axios from "axios";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const items = await axios.get(
+          "http://localhost:5000/api/v1/getProducts",
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        setProducts(items.data);
+        console.log(items);
+        console.log("Successfully received products!");
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
   const renderFeatured = () => {
     return products.map((product) => {
       return (
         <div>
           <div className="col mb-5">
             <div className="card h-100">
-              <img src={product.image} className="card-img-top" alt="..." />
+              <img src={parrotImage} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{product.title}</h5>
                 <p className="card-text">{product.description}</p>
+                <p className="card-text">${product.price}</p>
                 <Link to={"products/2"} class="btn btn-primary">
                   View details
                 </Link>
+                <button className="btn btn-primary mx-2"> Add to cart</button>
               </div>
             </div>
           </div>
