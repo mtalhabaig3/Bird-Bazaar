3bc640a2ba71a3c92da40e91e826ff7421778357
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-09-08 22:44:48 +0500

    feature - built a schema model for order

---
+++ backend/db/models/Order.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+        title: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      zipCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+  },
+  {
+    timestamps: true, // This will automatically manage `createdAt` and `updatedAt` for you
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
