30e197d75cf4af17558adbf8aa3cfcc51d345d5f
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-09-11 14:41:01 +0500

    feature - added a modal upon placing order

--- frontend/src/components/AddressPayment.js
+++ frontend/src/components/AddressPayment.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import Modal from "react-modal";
 import React, { useState } from "react";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    // right: "auto",
+    // bottom: "auto",
+    // marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 const AddressPayment = () => {
+  const [showModal, setShowModal] = useState(false);
   const [formData, setFormData] = useState({
     address: "",
     address_2: "",
@@ -44,105 +57,130 @@ const AddressPayment = () => {
     }
   };
   return (
-    <div className="container mt-5">
-      <h1 className="mb-5">Address and Payment</h1>
-      <form className="row g-3" onSubmit={(e) => handleSubmit(e)}>
-        <div className="col-12">
-          <label for="inputAddress" className="form-label">
-            Address
-          </label>
-          <input
-            name="address"
-            type="text"
-            className="form-control"
-            id="inputAddress"
-            placeholder="1234 Main St"
-            value={formData.address}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-12">
-          <label for="inputAddress2" className="form-label">
-            Address 2
-          </label>
-          <input
-            name="address_2"
-            type="text"
-            className="form-control"
-            id="inputAddress2"
-            placeholder="Apartment, studio, or floor"
-            value={formData.address_2}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-md-6">
-          <label for="inputCity" className="form-label">
-            City
-          </label>
-          <input
-            name="city"
-            type="text"
-            className="form-control"
-            id="inputCity"
-            value={formData.city}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-md-4">
-          <label for="inputState" className="form-label">
-            Country
-          </label>
-          <select
-            name="country"
-            id="inputcountry"
-            className="form-select"
-            value={formData.country}
-            onChange={handleChange}
-          >
-            <option selected> ...</option>
-            <option>Pakistan</option>
-            <option>Afghanistan</option>
-            <option>USA</option>
-          </select>
-        </div>
-        <div className="col-md-2">
-          <label for="inputZip" className="form-label">
-            Zip
-          </label>
-          <input
-            name="zip"
-            type="text"
-            className="form-control"
-            id="inputZip"
-            value={formData.zip}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-md-4">
-          <label for="inputState" className="form-label">
-            Payment Method
-          </label>
-          <select
-            name="payment"
-            id="inputcountry"
-            className="form-select"
-            value={formData.payment}
-            onChange={handleChange}
-          >
-            <option selected>...</option>
-            <option>Card</option>
-            <option>Cash On Delivery</option>
-            <option>Paypal</option>
-          </select>
-        </div>
+    <>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        contentLabel="Example Modal"
+        style={customStyles}
+      >
+        <h2>Orders</h2>
 
-        <div className="col-12">
-          <button type="submit" className="btn btn-primary">
-            Place Order
-          </button>
-        </div>
-      </form>
-    </div>
+        <div>Order placed!</div>
+        <button className="btn btn-primary" onClick={() => setShowModal(false)}>
+          View Orders
+        </button>
+        <button
+          className="btn btn-primary mx-5"
+          onClick={() => setShowModal(false)}
+        >
+          Go to home
+        </button>
+      </Modal>
+      <div className="container mt-5">
+        <h1 className="mb-5">Address and Payment</h1>
+        <form className="row g-3" onSubmit={(e) => handleSubmit(e)}>
+          <div className="col-12">
+            <label for="inputAddress" className="form-label">
+              Address
+            </label>
+            <input
+              name="address"
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+              value={formData.address}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="col-12">
+            <label for="inputAddress2" className="form-label">
+              Address 2
+            </label>
+            <input
+              name="address_2"
+              type="text"
+              className="form-control"
+              id="inputAddress2"
+              placeholder="Apartment, studio, or floor"
+              value={formData.address_2}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="col-md-6">
+            <label for="inputCity" className="form-label">
+              City
+            </label>
+            <input
+              name="city"
+              type="text"
+              className="form-control"
+              id="inputCity"
+              value={formData.city}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="col-md-4">
+            <label for="inputState" className="form-label">
+              Country
+            </label>
+            <select
+              name="country"
+              id="inputcountry"
+              className="form-select"
+              value={formData.country}
+              onChange={handleChange}
+            >
+              <option selected> ...</option>
+              <option>Pakistan</option>
+              <option>Afghanistan</option>
+              <option>USA</option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label for="inputZip" className="form-label">
+              Zip
+            </label>
+            <input
+              name="zip"
+              type="text"
+              className="form-control"
+              id="inputZip"
+              value={formData.zip}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="col-md-4">
+            <label for="inputState" className="form-label">
+              Payment Method
+            </label>
+            <select
+              name="payment"
+              id="inputcountry"
+              className="form-select"
+              value={formData.payment}
+              onChange={handleChange}
+            >
+              <option selected>...</option>
+              <option>Card</option>
+              <option>Cash On Delivery</option>
+              <option>Paypal</option>
+            </select>
+          </div>
+
+          <div className="col-12">
+            <button
+              onClick={() => setShowModal(true)}
+              type="submit"
+              className="btn btn-primary"
+            >
+              Place Order
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
